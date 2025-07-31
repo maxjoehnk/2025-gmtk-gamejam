6750598fce9e32b0a7e7a6cc25ce9bb4c035351b
Philip Reichenberg <s_ksfo6n@uni-bremen.de>
2025-07-31 20:21:16 +0200

    Better Guard Movement
    # Conflicts:
    #	Scenes/Enemies/Guard.cs

--- Scenes/Enemies/Guard.cs
+++ Scenes/Enemies/Guard.cs
@@ -4,28 +4,60 @@ using Godot;
 
 public partial class Guard : PathFollow2D
 {
-	private ActionPlayer Playback => ActionPlayer.Get(this);
+  private ActionPlayer Playback => ActionPlayer.Get(this);
 
-	[Export] public float Speed { get; set; } = 128;
+  [Export] public float Speed { get; set; } = 128;
 
-	[Signal]
-	public delegate void CatchedPlayerEventHandler();
+  [Export] public double InitialPoint { get; set; } = 4;
 
-	private CharacterBody2D Character => GetNode<CharacterBody2D>("CharacterBody2D");
+  [Signal]
+  public delegate void CatchedPlayerEventHandler();
 
-	public override void _Ready()
-	{
-		Playback.Ticked += OnTick;
-	}
+  private CharacterBody2D Character => GetNode<CharacterBody2D>("CharacterBody2D");
 
-	private void OnTick(int tick)
-	{
-		this.Progress = this.Speed * tick;
-	}
+  private int targetPoint = 0;
+  private int lastPoint = 0;
+  private double currentSubPoint;
 
-	public void OnHitPlayer(Node2D player)
-	{
-		GD.Print($"Hit player {player} ({player.Name})");
-		this.EmitSignalCatchedPlayer();
-	}
+  public override void _Ready()
+  {
+    Playback.Ticked += OnTick;
+
+    this.targetPoint = (int)this.InitialPoint;
+    this.lastPoint = (int)this.InitialPoint;
+    this.currentSubPoint = this.InitialPoint;
+  }
+
+  private void OnTick(int tick)
+  {
+    this.targetPoint = (int)this.InitialPoint + tick; // replace with waittime
+    if(tick == 0)
+    {
+      this.targetPoint = (int)this.InitialPoint;
+      this.lastPoint = (int)this.InitialPoint;
+      this.currentSubPoint = this.InitialPoint;
+      this.Progress = 0;
+    }
+  }
+
+  public void OnHitPlayer(Node2D player)
+  {
+    GD.Print($"Hit player {player} ({player.Name})");
+    this.EmitSignalCatchedPlayer();
+  }
+
+  public override void _Process(double delta)
+  {
+    this.currentSubPoint += delta;
+    if(currentSubPoint > 1)
+    {
+      this.lastPoint = Math.Min(this.lastPoint + 1, this.targetPoint);
+      this.currentSubPoint = this.currentSubPoint % 1;
+    }
+    if(this.lastPoint == this.targetPoint)
+      this.currentSubPoint = 0;
+    double ratio = Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi* this.currentSubPoint) * 0.5 + 0.5;
+    double interpolatedPoint = this.lastPoint * (1 - ratio) + Math.Min(this.lastPoint + 1, this.targetPoint) * ratio;
+    this.Progress = this.Speed* (float)interpolatedPoint;
+  }
 }

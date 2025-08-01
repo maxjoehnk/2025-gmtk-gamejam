dcf5e6e510b5e5d255798c091d8e2c5d2205ad67
Philip Reichenberg <s_ksfo6n@uni-bremen.de>
2025-08-01 17:31:08 +0200

    Fix player movement and tick logic when speed changes
    Guard TBD

--- Core/ActionPlayer.cs
+++ Core/ActionPlayer.cs
@@ -7,96 +7,109 @@ namespace gmtkgamejam.Scenes;
 
 public partial class ActionPlayer : Node
 {
-	public static ActionPlayer Get(Node node)
-	{
-		return (ActionPlayer)node.GetTree().GetFirstNodeInGroup("ActionPlayer");
-	}
+  public static ActionPlayer Get(Node node)
+  {
+    return (ActionPlayer)node.GetTree().GetFirstNodeInGroup("ActionPlayer");
+  }
 
-	[Signal]
-	public delegate void FinishedEventHandler();
+  [Signal]
+  public delegate void FinishedEventHandler();
 
-	[Signal]
-	public delegate void TickedEventHandler(int tick);
+  [Signal]
+  public delegate void TickedEventHandler(int tick);
 
-	private Timer? Timer => this.GetNodeOrNull<Timer>("Timer");
+  private Timer ? Timer => this.GetNodeOrNull<Timer>("Timer");
 
-	[Export]
-	public double PlaybackSpeed
-	{
-		get => 1 / (this.Timer?.WaitTime ?? 1);
-		set
-		{
-			if (this.Timer == null)
-			{
-				return;
-			}
+  [Export]
+  public double PlaybackSpeed
+  {
+    get => 1 / (this.Timer?.WaitTime ?? 1);
+    set
+    {
+      if(this.Timer == null)
+      {
+        return;
+      }
 
-			this.Timer.WaitTime = 1 / value;
-			GD.Print($"Set TickDuration to {this.Timer.WaitTime}s");
-		}
-	}
+      this.Timer.WaitTime = 1 / value;
+      GD.Print($"Set TickDuration to {this.Timer.WaitTime}s");
+    }
+  }
 
-	private Array<Action> Actions { get; set; }
+  private Array<Action> Actions { get; set; }
 
-	private int ActionIndex { get; set; }
+  private int ActionIndex { get; set; }
 
-	public int CurrentTick { get; private set; }
+  public int CurrentTick { get; private set; }
 
-	private int ActionTicksRemaining { get; set; }
+  private int ActionTicksRemaining { get; set; }
 
-	public double TickDuration => this.Timer?.WaitTime ?? 1;
+  public double TickDuration => this.Timer?.WaitTime ?? 1;
 
-	private Action? CurrentAction => Actions.ElementAtOrDefault(ActionIndex);
+  private Action ? CurrentAction => Actions.ElementAtOrDefault(ActionIndex);
 
+  private double executedWaitTime = 0;
 
-	public void Play(Array<Action> actions)
-	{
-		Actions = actions;
-		ActionTicksRemaining = CurrentAction?.Ticks ?? 0;
+  public void Play(Array<Action> actions)
+  {
+    Actions = actions;
+    ActionTicksRemaining = CurrentAction?.Ticks ?? 0;
 
-		Tick();
-	}
+    Tick();
+  }
 
-	public void Stop()
-	{
-		this.Timer?.Stop();
-	}
+  public void Stop()
+  {
+    this.Timer?.Stop();
+  }
 
-	public void Tick()
-	{
-		if (CurrentAction == null)
-		{
-			return;
-		}
+  public void Tick()
+  {
+    if(executedWaitTime < TickDuration)
+    {
+      this.Timer?.Start();
+      return;
+    }
+    if(CurrentAction == null)
+    {
+      return;
+    }
 
-		CurrentTick += 1;
-		EmitSignalTicked(CurrentTick);
-		CurrentAction?.Act(GetParent<Game>().Player);
-		ActionTicksRemaining -= 1;
-		if (ActionTicksRemaining <= 0)
-		{
-			NextAction();
-		}
+    CurrentTick += 1;
+    EmitSignalTicked(CurrentTick);
+    CurrentAction?.Act(GetParent<Game>().Player);
+    ActionTicksRemaining -= 1;
+    if(ActionTicksRemaining <= 0)
+    {
+      NextAction();
+    }
 
-		this.Timer?.Start();
-	}
+    this.Timer?.Start();
 
-	private void NextAction()
-	{
-		this.ActionIndex += 1;
-		this.ActionTicksRemaining = CurrentAction?.Ticks ?? 0;
-		if (ActionIndex >= Actions.Count)
-		{
-			EmitSignalFinished();
-		}
-	}
+    executedWaitTime = 0;
+  }
 
-	public void Reset()
-	{
-		this.Stop();
-		CurrentTick = 0;
-		ActionIndex = 0;
-		ActionTicksRemaining = 0;
-		EmitSignalTicked(CurrentTick);
-	}
+  private void NextAction()
+  {
+    this.ActionIndex += 1;
+    this.ActionTicksRemaining = CurrentAction?.Ticks ?? 0;
+    if(ActionIndex >= Actions.Count)
+    {
+      EmitSignalFinished();
+    }
+  }
+
+  public void Reset()
+  {
+    this.Stop();
+    CurrentTick = 0;
+    ActionIndex = 0;
+    ActionTicksRemaining = 0;
+    EmitSignalTicked(CurrentTick);
+  }
+
+  public override void _Process(double delta)
+  {
+    executedWaitTime += delta;
+  }
 }
--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -2,18 +2,20 @@ using Godot;
 using gmtkgamejam.Core;
 using gmtkgamejam.Scenes;
 using Vector2 = Godot.Vector2;
+using static Godot.TextServer;
 
 public partial class Player : CharacterBody2D
 {
   private const int GridSize = 128;
 
-  [Export] public float RotationSpeed { get; set; } = Mathf.Pi * 2 * 180 / Mathf.Pi;
+  [Export] public float RotationSpeed { get; set; } = 360;
 
   private ActionPlayer Playback => ActionPlayer.Get(this);
   private Vector2 nextDirection = new Vector2(0, 0);
   private float nextRotation = 0;
   private float currentRotation = 0;
   private double executedTime = 0;
+  private double lastExecutedRatio = 0;
 
   public void Move(MoveDirection direction)
   {
@@ -27,6 +29,8 @@ public partial class Player : CharacterBody2D
       nextDirection = new Vector2(0, 0);
       return;
     }
+
+    lastExecutedRatio = 0;
   }
 
   public override void _Process(double delta)
@@ -37,19 +41,19 @@ public partial class Player : CharacterBody2D
     double lastExecutedTime = executedTime;
     executedTime += delta;
 
-    float maxRotationPerUpdate = this.RotationSpeed* (float)delta;
+    float maxRotationPerUpdate = this.RotationSpeed* (float)delta / (float)this.Playback.TickDuration;
     currentRotation += limit(nextRotation - currentRotation, maxRotationPerUpdate);
 
-    double lastBaseRatio = Mathf.Min(1, lastExecutedTime / this.Playback.TickDuration);
     double baseRatio = Mathf.Min(1, this.executedTime / this.Playback.TickDuration);
 
-    double lastRatioTranslation = Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi* lastBaseRatio) * 0.5 + 0.5;
+    double lastRatioTranslation = Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi* lastExecutedRatio) * 0.5 + 0.5;
     double ratioTranslation = Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi* baseRatio) * 0.5 + 0.5;
 
     float ratioDiff = (float)(ratioTranslation - lastRatioTranslation);
     Vector2 movement = new Vector2(this.nextDirection.X* ratioDiff, this.nextDirection.Y* ratioDiff);
     MoveAndCollide(movement);
     this.RotationDegrees = currentRotation;
+    lastExecutedRatio = baseRatio;
   }
 
   private static float limit(float val, float max)

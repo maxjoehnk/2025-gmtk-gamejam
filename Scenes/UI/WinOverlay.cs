98e21037bcfe02b0887adda07158eac114b50d30
Max Jöhnk <maxjoehnk@gmail.com>
2025-08-01 16:21:50 +0200

    Rework Level Cleared Overlay

--- Scenes/UI/WinOverlay.cs
+++ Scenes/UI/WinOverlay.cs
@@ -1,13 +1,14 @@
 using Godot;
 using System;
+using gmtkgamejam.Core;
 
 public partial class WinOverlay : Control
 {
-	private Label Title => this.GetNode<Label>("Dialog/MarginContainer/VBoxContainer/Title");
-	private Label Ticks => this.GetNode<Label>("Dialog/MarginContainer/VBoxContainer/TickCounter");
-	private Control GoldMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/Medals/Gold Medal");
-	private Control SilverMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/Medals/Silver Medal");
-	private Control BronzeMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/Medals/Bronze Medal");
+	private Label Title => this.GetNode<Label>("Dialog/MarginContainer/VBoxContainer/Title Container/Title");
+	private Label Ticks => this.GetNode<Label>("Dialog/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer/TickCounter");
+	private Control GoldMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/HBoxContainer2/Medals/Gold Medal");
+	private Control SilverMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/HBoxContainer2/Medals/Silver Medal");
+	private Control BronzeMedalIndicator => this.GetNode<Control>("Dialog/MarginContainer/VBoxContainer/HBoxContainer2/Medals/Bronze Medal");
 	
 	[Signal]
 	public delegate void RestartLevelEventHandler();
@@ -18,7 +19,7 @@ public partial class WinOverlay : Control
 	public void Open(string name, int tickCount, bool hasGoldMedal, bool hasSilverMedal, bool hasBronzeMedal)
 	{
 		this.Title.Text = name;
-		this.Ticks.Text = $"Took {tickCount} Ticks";
+		this.Ticks.Text = $"{tickCount} Ticks";
 		this.GoldMedalIndicator.Visible = hasGoldMedal;
 		this.SilverMedalIndicator.Visible = hasSilverMedal;
 		this.BronzeMedalIndicator.Visible = hasBronzeMedal;
@@ -33,5 +34,10 @@ public partial class WinOverlay : Control
 	public void OnNextPressed()
 	{
 		this.EmitSignalNextLevel();
+	}
+
+	public void OnExitPressed()
+	{
+		LevelLoader.Instance.OpenMainMenu();
 	}
 }

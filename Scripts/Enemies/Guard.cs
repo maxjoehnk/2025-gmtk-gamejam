d0a9839dfba5782b3acbe4aeecdcaa3353056400
Max Jöhnk <maxjoehnk@gmail.com>
2025-08-01 21:10:08 +0200

    Add spikes object and move assets into more folders

--- Scripts/Enemies/Guard.cs
+++ Scripts/Enemies/Guard.cs
@@ -4,61 +4,67 @@ using Godot;
 
 public partial class Guard : PathFollow2D
 {
-  private ActionPlayer Playback => ActionPlayer.Get(this);
+	private ActionPlayer Playback => ActionPlayer.Get(this);
 
-  [Export] public float Speed { get; set; } = 128;
+	[Export] public float Speed { get; set; } = 128;
 
-  [Export] public int StartPosition { get; set; } = 0;
+	[Export] public int StartPosition { get; set; } = 0;
 
-  [Signal]
-  public delegate void CatchedPlayerEventHandler();
+	[Signal]
+	public delegate void CatchedPlayerEventHandler();
 
-  private CharacterBody2D Character => GetNode<CharacterBody2D>("CharacterBody2D");
+	private CharacterBody2D Character => GetNode<CharacterBody2D>("CharacterBody2D");
 
-  private int targetPoint = 0;
-  private int lastPoint = 0;
-  private double currentSubTime = 0;
+	private int targetPoint = 0;
+	private int lastPoint = 0;
+	private double currentSubTime = 0;
 
-  public override void _Ready()
-  {
-    Playback.Ticked += OnTick;
+	public override void _Ready()
+	{
+		Playback.Ticked += OnTick;
 
-    this.targetPoint = this.StartPosition;
-    this.lastPoint = this.StartPosition;
-    this.currentSubTime = 0;
-    this.Progress = this.StartPosition;
-  }
+		this.targetPoint = this.StartPosition;
+		this.lastPoint = this.StartPosition;
+		this.currentSubTime = 0;
+		this.Progress = this.StartPosition;
+	}
 
-  private void OnTick(int tick)
-  {
-    this.targetPoint = this.StartPosition + tick; // replace with waittime
-    if(tick == 0)
-    {
-      this.targetPoint = this.StartPosition;
-      this.lastPoint = this.StartPosition;
-      this.currentSubTime = this.StartPosition;
-      this.Progress = this.StartPosition;
-    }
-  }
+	private void OnTick(int tick)
+	{
+		this.targetPoint = this.StartPosition + tick; // replace with waittime
+		if (tick == 0)
+		{
+			this.targetPoint = this.StartPosition;
+			this.lastPoint = this.StartPosition;
+			this.currentSubTime = this.StartPosition;
+			this.Progress = this.StartPosition;
+		}
+	}
 
-  public void OnHitPlayer(Node2D player)
-  {
-    GD.Print($"Hit player {player} ({player.Name})");
-    this.EmitSignalCatchedPlayer();
-  }
+	public void OnHitPlayer(Node2D player)
+	{
+		GD.Print($"Hit object {player} ({player.Name})");
+		if (player is Player)
+		{
+			this.EmitSignalCatchedPlayer();
+		}
+	}
 
-  public override void _Process(double delta)
-  {
-    this.currentSubTime += delta;
-    if(currentSubTime > this.Playback.TickDuration)
-    {
-      this.lastPoint = Math.Min(this.lastPoint + 1, this.targetPoint);
-      this.currentSubTime -= this.Playback.TickDuration;
-    }
-    if(this.lastPoint == this.targetPoint)
-      this.currentSubTime = 0;
-    double ratio = Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi* Mathf.Min(1, this.currentSubTime / this.Playback.TickDuration)) * 0.5 + 0.5;
-    double interpolatedPoint = this.lastPoint * (1 - ratio) + Math.Min(this.lastPoint + 1, this.targetPoint) * ratio;
-    this.Progress = this.Speed* (float)interpolatedPoint;
-  }
+	public override void _Process(double delta)
+	{
+		this.currentSubTime += delta;
+		if (currentSubTime > this.Playback.TickDuration)
+		{
+			this.lastPoint = Math.Min(this.lastPoint + 1, this.targetPoint);
+			this.currentSubTime -= this.Playback.TickDuration;
+		}
+
+		if (this.lastPoint == this.targetPoint)
+			this.currentSubTime = 0;
+		double ratio =
+			Mathf.Sin(-Mathf.Pi * 0.5 + Mathf.Pi * Mathf.Min(1, this.currentSubTime / this.Playback.TickDuration)) * 0.5 +
+			0.5;
+		double interpolatedPoint = this.lastPoint * (1 - ratio) + Math.Min(this.lastPoint + 1, this.targetPoint) * ratio;
+		this.Progress = this.Speed * (float)interpolatedPoint;
+	}
 }
---
+++ Scripts/Objects/Spikes.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+using gmtkgamejam.Scripts.Core;
+
+public partial class Spikes : Node2D, ISwitchable, IResettable
+{
+	private StaticBody2D Collider => this.GetNode<StaticBody2D>("Spikes");
+	private Sprite2D Collapsed => this.GetNode<Sprite2D>("Spikes/Spikes");
+	private Sprite2D Expanded => this.GetNode<Sprite2D>("Spikes/SpikesExpanded");
+
+	[Export] public bool IsActive { get; set; } = true;
+	
+	private bool state = true;
+
+	public override void _Process(double delta)
+	{
+		GD.Print("Spikes _Process called while state is " + this.state);
+		this.Collapsed.Visible = !this.state;
+		this.Expanded.Visible = this.state;
+		this.Collider.ProcessMode = this.state ? ProcessModeEnum.Inherit : ProcessModeEnum.Disabled;
+	}
+
+	public override void _Ready()
+	{
+		this.state = IsActive;
+	}
+
+	public void Toggle()
+	{
+		this.state = !this.state;
+	}
+
+	public void Reset()
+	{
+		this.state = IsActive;
+	}
+}

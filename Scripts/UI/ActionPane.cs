a7ac9d7cf432dcea817be9b0599980f5b4e88f05
NoNa <43652979+kvncze@users.noreply.github.com>
2025-08-01 19:02:22 +0200

    added collapse button

--- Scripts/UI/ActionPane.cs
+++ Scripts/UI/ActionPane.cs
@@ -5,147 +5,169 @@ using Godot.Collections;
 
 public partial class ActionPane : PanelContainer
 {
-    private PackedScene MovementActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/MovementAction.tscn");
-    private PackedScene InteractActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/InteractAction.tscn");
-    private PackedScene WaitActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/WaitAction.tscn");
-    private PackedScene ActionEntryScene => GD.Load<PackedScene>("res://Scenes/UI/ActionMenu/ActionEntry.tscn");
-    
-    private VBoxContainer ActionList => GetNode<VBoxContainer>("MainVBox/ScrollContainer/ActionList");
-    private Button AddActionButton => GetNode<Button>("MainVBox/AddActionButton");
-    private PopupMenu ActionPopupMenu => GetNode<PopupMenu>("MainVBox/ActionPopupMenu");
+	private PackedScene MovementActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/MovementAction.tscn");
+	private PackedScene InteractActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/InteractAction.tscn");
+	private PackedScene WaitActionScene => GD.Load<PackedScene>("res://Scenes/UI/Actions/WaitAction.tscn");
+	private PackedScene ActionEntryScene => GD.Load<PackedScene>("res://Scenes/UI/ActionMenu/ActionEntry.tscn");
+	
+	private VBoxContainer ActionList => GetNode<VBoxContainer>("HBoxContainer/MainVBox/ScrollContainer/ActionList");
+	private Button AddActionButton => GetNode<Button>("HBoxContainer/MainVBox/AddActionButton");
+	private PopupMenu ActionPopupMenu => GetNode<PopupMenu>("HBoxContainer/MainVBox/ActionPopupMenu");
 
-    // ActionPaneUI Buttons
-    private TextureButton AddLeftButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Left");
-    private TextureButton AddUpButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Up");
-    private TextureButton AddDownButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Down");
-    private TextureButton AddRightButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Right");
-    private TextureButton AddInteractButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Interact");
-    private TextureButton AddWaitButton => GetNode<TextureButton>("MainVBox/HBoxContainer/Wait");
+	// ActionPaneUI Buttons
+	private TextureButton AddLeftButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Left");
+	private TextureButton AddUpButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Up");
+	private TextureButton AddDownButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Down");
+	private TextureButton AddRightButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Right");
+	private TextureButton AddInteractButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Interact");
+	private TextureButton AddWaitButton => GetNode<TextureButton>("HBoxContainer/MainVBox/HBoxContainer/Wait");
+	private Button CollapseButton => GetNode<Button>("HBoxContainer/CollapseButton");
+	private VBoxContainer MainVBox => GetNode<VBoxContainer>("HBoxContainer/MainVBox");
 
-    public Array<Action> Actions => new(ActionList.GetChildren().Cast<ActionEntry>().Select(a => a.Action));
-    
-    [Signal]
-    public delegate void ActionsChangedEventHandler();
 
-    public override void _Ready()
-    {
-        this.ActionList.ChildExitingTree += child =>
-        {
-            child.TreeExited += this.EmitSignalActionsChanged;
-        };
-        this.ActionList.ChildEnteredTree += child =>
-        {
-            if (child is ActionEntry entry)
-            {
-                entry.ActionChanged += this.EmitSignalActionsChanged;
-            }
-            this.EmitSignalActionsChanged();
-        };
-    }
+	public Array<Action> Actions => new(ActionList.GetChildren().Cast<ActionEntry>().Select(a => a.Action));
+	
+	[Signal]
+	public delegate void ActionsChangedEventHandler();
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (Input.IsActionJustPressed("Add_MoveLeft"))
-            OnLeftButton_Click();
+	public override void _Ready()
+	{
+		this.ActionList.ChildExitingTree += child =>
+		{
+			child.TreeExited += this.EmitSignalActionsChanged;
+		};
+		this.ActionList.ChildEnteredTree += child =>
+		{
+			if (child is ActionEntry entry)
+			{
+				entry.ActionChanged += this.EmitSignalActionsChanged;
+			}
+			this.EmitSignalActionsChanged();
+		};
+	}
 
-        if (Input.IsActionJustPressed("Add_MoveUp"))
-            OnUpButton_Click();
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (Input.IsActionJustPressed("Add_MoveLeft"))
+			OnLeftButton_Click();
 
-        if (Input.IsActionJustPressed("Add_MoveDown"))
-            OnDownButton_Click();
+		if (Input.IsActionJustPressed("Add_MoveUp"))
+			OnUpButton_Click();
 
-        if (Input.IsActionJustPressed("Add_MoveRight"))
-            OnRightButton_Click();
+		if (Input.IsActionJustPressed("Add_MoveDown"))
+			OnDownButton_Click();
 
-        if (Input.IsActionJustPressed("Add_Interact"))
-            OnInteractButton_Click();
+		if (Input.IsActionJustPressed("Add_MoveRight"))
+			OnRightButton_Click();
 
-        if (Input.IsActionJustPressed("Add_Wait"))
-            OnWaitButton_Click();
-    }
+		if (Input.IsActionJustPressed("Add_Interact"))
+			OnInteractButton_Click();
 
-    public void OnLeftButton_Click()
-    {
-        AddMoveAction(MoveDirection.Left);
-    }
+		if (Input.IsActionJustPressed("Add_Wait"))
+			OnWaitButton_Click();
+	}
 
-    public void OnUpButton_Click()
-    {
-        AddMoveAction(MoveDirection.Up);
-    }
+	public void OnCollapseButton_Click(bool isCollapsed)
+	{
+		Texture2D collapsedTrue = GD.Load<Texture2D>("res://Assets/chevron-left.png");
+		Texture2D collapsedFalse = GD.Load<Texture2D>("res://Assets/chevron-right.png");
 
-    public void OnDownButton_Click()
-    {
-        AddMoveAction(MoveDirection.Down);
-    }
+		MainVBox.Visible = !isCollapsed;
+		if (isCollapsed)
+		{
+			CollapseButton.Icon = collapsedFalse;
+			this.SelfModulate = new Color(255, 255, 255, 0);
+		}
+		else
+		{
+			CollapseButton.Icon = collapsedTrue;
+			this.SelfModulate = new Color("#ffffff");
+		}
+		GD.Print($"ActionPane: Collapsing: {isCollapsed}");
+	}
 
-    public void OnRightButton_Click()
-    {
-        AddMoveAction(MoveDirection.Right);
-    }
+	public void OnLeftButton_Click()
+	{
+		AddMoveAction(MoveDirection.Left);
+	}
 
-    public void OnInteractButton_Click()
-    {
-        AddInteractAction();
-    }
+	public void OnUpButton_Click()
+	{
+		AddMoveAction(MoveDirection.Up);
+	}
 
-    public void OnWaitButton_Click()
-    {
-        AddWaitAction();
-    }
+	public void OnDownButton_Click()
+	{
+		AddMoveAction(MoveDirection.Down);
+	}
 
-    private void AddAction(Action action)
-    {
-        ActionEntry entry = ActionEntryScene.Instantiate<ActionEntry>();
-        entry.Action = action;
+	public void OnRightButton_Click()
+	{
+		AddMoveAction(MoveDirection.Right);
+	}
 
-        GD.Print($"Added {entry.Action.Title}, with {entry.Action.Ticks} Ticks.");
+	public void OnInteractButton_Click()
+	{
+		AddInteractAction();
+	}
 
-        ActionList.AddChild(entry);
-    }
+	public void OnWaitButton_Click()
+	{
+		AddWaitAction();
+	}
 
-    //private void AddAction(Action action)
-    //{
-    //    int childCount = ActionList.GetChildCount();
+	private void AddAction(Action action)
+	{
+		ActionEntry entry = ActionEntryScene.Instantiate<ActionEntry>();
+		entry.Action = action;
 
-    //    if (childCount > 0)
-    //    {
-    //        Node lastChild = ActionList.GetChild(childCount - 1);
+		GD.Print($"Added {entry.Action.Title}, with {entry.Action.Ticks} Ticks.");
 
-    //        if (lastChild is ActionEntry lastEntry && lastEntry.Action.Title == action.Title)
-    //        {
-    //            lastEntry.Action.Ticks += action.Ticks; // z. B. +1
-    //            lastEntry.UpdateTickDisplay();
-    //            GD.Print($"Updated {lastEntry.Action.Title}, now {lastEntry.Action.Ticks} Ticks.");
-    //            return;
-    //        }
-    //    }
+		ActionList.AddChild(entry);
+	}
 
-    //    // Kein gleiches letztes Element → neues hinzufügen
-    //    ActionEntry newEntry = ActionEntryScene.Instantiate<ActionEntry>();
-    //    newEntry.Action = action;
-    //    newEntry.UpdateTickDisplay();
-    //    ActionList.AddChild(newEntry);
+	//private void AddAction(Action action)
+	//{
+	//    int childCount = ActionList.GetChildCount();
 
-    //    GD.Print($"Added {newEntry.Action.Title}, with {newEntry.Action.Ticks} Ticks.");
-    //}
+	//    if (childCount > 0)
+	//    {
+	//        Node lastChild = ActionList.GetChild(childCount - 1);
 
-    private void AddMoveAction(MoveDirection dir)
-    {
-        var action = MovementActionScene.Instantiate<MoveAction>();
-        action.Direction = dir;
-        AddAction(action);
-    }
+	//        if (lastChild is ActionEntry lastEntry && lastEntry.Action.Title == action.Title)
+	//        {
+	//            lastEntry.Action.Ticks += action.Ticks; // z. B. +1
+	//            lastEntry.UpdateTickDisplay();
+	//            GD.Print($"Updated {lastEntry.Action.Title}, now {lastEntry.Action.Ticks} Ticks.");
+	//            return;
+	//        }
+	//    }
 
-    private void AddInteractAction()
-    {
-        var action = InteractActionScene.Instantiate<InteractAction>();
-        AddAction(action);
-    }
+	//    // Kein gleiches letztes Element → neues hinzufügen
+	//    ActionEntry newEntry = ActionEntryScene.Instantiate<ActionEntry>();
+	//    newEntry.Action = action;
+	//    newEntry.UpdateTickDisplay();
+	//    ActionList.AddChild(newEntry);
 
-    private void AddWaitAction()
-    {
-        var action = WaitActionScene.Instantiate<WaitAction>();
-        AddAction(action);
-    }
+	//    GD.Print($"Added {newEntry.Action.Title}, with {newEntry.Action.Ticks} Ticks.");
+	//}
+
+	private void AddMoveAction(MoveDirection dir)
+	{
+		var action = MovementActionScene.Instantiate<MoveAction>();
+		action.Direction = dir;
+		AddAction(action);
+	}
+
+	private void AddInteractAction()
+	{
+		var action = InteractActionScene.Instantiate<InteractAction>();
+		AddAction(action);
+	}
+
+	private void AddWaitAction()
+	{
+		var action = WaitActionScene.Instantiate<WaitAction>();
+		AddAction(action);
+	}
 }

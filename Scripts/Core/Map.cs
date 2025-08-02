9c7f52facb5ad4cd2c010e98070ae571353df569
Philip Reichenberg <s_ksfo6n@uni-bremen.de>
2025-08-02 16:05:36 +0200

    Fix getting caught when resetting

--- Scripts/Core/Map.cs
+++ Scripts/Core/Map.cs
@@ -6,50 +6,57 @@ using Godot.Collections;
 
 public partial class Map : Node
 {
-	public Vector2 SpawnPosition => this.GetNode<Node2D>("TileMapLayer/Spawn").GlobalPosition;
-	public Game Game => this.GetNode<Game>("Game");
+  public Vector2 SpawnPosition => this.GetNode<Node2D>("TileMapLayer/Spawn").GlobalPosition;
+  public Game Game => this.GetNode<Game>("Game");
 
-	[Export] public int GoldMedalTicks { get; set; }
+  [Export] public int GoldMedalTicks { get; set; }
 
-	[Export] public int SilverMedalTicks { get; set; }
+  [Export] public int SilverMedalTicks { get; set; }
 
-	[Export] public int BronzeMedalTicks { get; set; }
-	
-	[Signal]
-	public delegate void PlayerLostEventHandler();
-	
-	[Signal]
-	public delegate void PlayerWonEventHandler();
+  [Export] public int BronzeMedalTicks { get; set; }
 
-	public override void _Ready()
-	{
-		Array<Node> enemies = this.GetTree().GetNodesInGroup(Groups.Enemies);
-		foreach (Node enemy in enemies)
-		{
-			GD.Print($"Connecting catched player signal from {enemy} ({enemy.Name})");
-			enemy.Connect(nameof(Enemy.CatchedPlayer), Callable.From(this.OnPlayerCaught));
-		}
-		
-		Array<Node> exits = this.GetTree().GetNodesInGroup(Groups.LevelExits);
-		foreach (Node exit in exits)
-		{
-			exit.Connect(nameof(LevelGoal.PlayerReachedGoal), Callable.From(this.OnPlayerWon));
-		}
+  [Signal]
+  public delegate void PlayerLostEventHandler();
 
-		this.Game.SpawnPosition = this.SpawnPosition;
-		this.Game.Respawn();
-		this.Game.Show();
-	}
+  [Signal]
+  public delegate void PlayerWonEventHandler();
 
-	private void OnPlayerCaught()
-	{
-		GD.Print("Player caught");
-		this.Game.OnPlayerLost(this.Name);
-	}
+  public override void _Ready()
+  {
+    Array<Node> enemies = this.GetTree().GetNodesInGroup(Groups.Enemies);
+    foreach(Node enemy in enemies)
+    {
+      GD.Print($"Connecting catched player signal from {enemy} ({enemy.Name})");
+      enemy.Connect(nameof(Enemy.CatchedPlayer), Callable.From(this.OnPlayerCaught));
+    }
 
-	public void OnPlayerWon()
-	{
-		GD.Print("Player won");
-		this.Game.OnPlayerWon(this.Name, this.GoldMedalTicks, this.SilverMedalTicks, this.BronzeMedalTicks);
-	}
+    Array<Node> exits = this.GetTree().GetNodesInGroup(Groups.LevelExits);
+    foreach(Node exit in exits)
+    {
+      exit.Connect(nameof(LevelGoal.PlayerReachedGoal), Callable.From(this.OnPlayerWon));
+    }
+
+    this.Game.SpawnPosition = this.SpawnPosition;
+    this.Game.Respawn();
+    this.Game.Show();
+  }
+
+  private void OnPlayerCaught()
+  {
+    GD.Print(this.Game.CurrentGameState);
+    if(this.Game.CurrentGameState == gmtkgamejam.Core.GameStates.Playing)
+    {
+      GD.Print("Player caught");
+      this.Game.OnPlayerLost(this.Name);
+    }
+  }
+
+  public void OnPlayerWon()
+  {
+    if(this.Game.CurrentGameState == gmtkgamejam.Core.GameStates.Playing)
+    {
+      GD.Print("Player won");
+      this.Game.OnPlayerWon(this.Name, this.GoldMedalTicks, this.SilverMedalTicks, this.BronzeMedalTicks);
+    }
+  }
 }

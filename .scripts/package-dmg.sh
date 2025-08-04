7e5c40e52b657330b86687c71a92b2da5878795a
Max Jöhnk <maxjoehnk@gmail.com>
2025-08-04 09:51:35 +0200

    Fix typo in notarization & signing scripts

--- .scripts/package-dmg.sh
+++ .scripts/package-dmg.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo "Packaging as .dmg..."
-create-dmg --volname Loopback \
-	--volicon "build/Loopback.app/Contents/Resources/icon.icns" \
+create-dmg --volname Loopscape \
+	--volicon "build/Loopscape.app/Contents/Resources/icon.icns" \
 	--window-pos 200 120 \
 	--window-size 800 400 \
 	--icon-size 100 \
-	--icon "Loopback.app" 200 190 \
-	--hide-extension "Loopback.app" \
+	--icon "Loopscape.app" 200 190 \
+	--hide-extension "Loopscape.app" \
 	--app-drop-link 600 185 \
 	--codesign "$MACOS_CERTIFICATE_NAME" \
-	Loopback.dmg \
+	Loopscape.dmg \
 	build

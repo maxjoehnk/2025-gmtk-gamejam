ab1c8d9e21a6b1fbc19bdb344e5fc3d91e470ef2
Max Jöhnk <maxjoehnk@gmail.com>
2025-08-04 08:45:05 +0200

    Sign and notarize macos build

---
+++ .scripts/package-dmg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Packaging as .dmg..."
+create-dmg --volname Loopback \
+	--volicon "export/mac/Loopback.app/Contents/Resources/icon.icns" \
+	--window-pos 200 120 \
+	--window-size 800 400 \
+	--icon-size 100 \
+	--icon "Loopback.app" 200 190 \
+	--hide-extension "Loopback.app" \
+	--app-drop-link 600 185 \
+	--codesign "$MACOS_CERTIFICATE_NAME" \
+	Loopback.dmg \
+	export/mac
